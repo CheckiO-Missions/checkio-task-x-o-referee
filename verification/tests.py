c6059df1a32fabfe9628c855db3605a2177de2da
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-02-28 19:48:34 +1200

    new tests

--- verification/tests.py
+++ verification/tests.py
@@ -8,7 +8,7 @@ Each test is dict with
 """
 
 TESTS = {
-    "Basics": [
+    "1. Basics": [
         {
             "input": ["X.O",
                       "XX.",
@@ -31,15 +31,14 @@ TESTS = {
             "explanation": []
         },
         {
-            "input": [
-                "O.X",
-                "XX.",
-                "XOO"],
+            "input": ["O.X",
+                      "XX.",
+                      "XOO"],
             "answer": "X",
             "explanation": [1, [3, 0], [0, 3]]
         },
     ],
-    "Extra": [
+    "2. Extra": [
         {
             "input": ["OOO",
                       "XX.",
@@ -81,6 +80,79 @@ TESTS = {
                       'OO.'],
             "answer": "X",
             "explanation": [1, [1.5, 0], [1.5, 3]]
+        },
+    ],
+    "3. Extra": [
+
+        {
+            "input": ['OOO',
+                      'X.X',
+                      '.X.'],
+            "answer": "O",
+            "explanation": [0, [0.5, 0], [0.5, 3]]
+        },
+        {
+            "input": ['O..',
+                      'XOX',
+                      '..O'],
+            "answer": "O",
+            "explanation": [0, [0, 0], [3, 3]]
+        },
+        {
+            "input": ['..O',
+                      'XOX',
+                      'O..'],
+            "answer": "O",
+            "explanation": [0, [0, 3], [3, 0]]
+        },
+        {
+            "input": ['.XO',
+                      'X.X',
+                      'OOO'],
+            "answer": "O",
+            "explanation": [0, [2.5, 0], [2.5, 3]]
+        },
+        {
+            "input": ['.XO',
+                      'X.X',
+                      'O.O'],
+            "answer": "D",
+            "explanation": []
+        },
+        {
+            "input": [".OX",
+                      ".XX",
+                      ".OO"],
+            "answer": "D",
+            "explanation": []
+        },
+        {
+            "input": ["...",
+                      ".X.",
+                      "..."],
+            "answer": "D",
+            "explanation": []
+        },
+        {
+            "input": [".O.",
+                      ".X.",
+                      "..."],
+            "answer": "D",
+            "explanation": []
+        },
+        {
+            "input": [".O.",
+                      "...",
+                      "..."],
+            "answer": "D",
+            "explanation": []
+        },
+        {
+            "input": [".OX",
+                      "..X",
+                      ".OX"],
+            "answer": "X",
+            "explanation": [1, [0, 2.5], [3, 2.5]]
         },
 
 
